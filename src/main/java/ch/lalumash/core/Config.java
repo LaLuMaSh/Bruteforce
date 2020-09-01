c0b348cce5d787f39fc9c5879dedf31a02c43776
LucaGuettinger <luca.guettinger@cmiag.ch>
2020-09-01 08:20:52 +0200

    remove lombok

--- src/main/java/ch/lalumash/core/Config.java
+++ src/main/java/ch/lalumash/core/Config.java
@@ -6,13 +6,61 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Config implements Serializable {
     private String url;
     private String symbols;
     private String max;
     private String user;
     private String dtosPerRequest;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(String symbols) {
+        this.symbols = symbols;
+    }
+
+    public String getMax() {
+        return max;
+    }
+
+    public void setMax(String max) {
+        this.max = max;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDtosPerRequest() {
+        return dtosPerRequest;
+    }
+
+    public void setDtosPerRequest(String dtosPerRequest) {
+        this.dtosPerRequest = dtosPerRequest;
+    }
+
+    public Config() {
+    }
+
+    public Config(String url, String symbols, String max, String user, String dtosPerRequest) {
+        this.url = url;
+        this.symbols = symbols;
+        this.max = max;
+        this.user = user;
+        this.dtosPerRequest = dtosPerRequest;
+    }
 }
--- src/main/java/ch/lalumash/core/LoginResponseDto.java
+++ src/main/java/ch/lalumash/core/LoginResponseDto.java
@@ -4,9 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
 public class LoginResponseDto {
     String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LoginResponseDto() {
+    }
+
+    public LoginResponseDto(String token) {
+        this.token = token;
+    }
 }
